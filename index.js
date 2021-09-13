57895a8668410755bc34cf4b8a2c2a5ec0b6e0ed
Jonathan <jonkimyagarov@gmail.com>
2021-09-13 00:14:13 -0400

    commit tree 3c72eff3eb448e68829638bbe85293de12fce0dd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Follow along with the examples here
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Isabel"); // "Hello, Isabel!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+  
